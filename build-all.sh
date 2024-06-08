da7c1ffd3076c587ac48b3b416eef5781bf0394e
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-06-08 09:09:22 -0700

    Auto-format 'build-all.sh'

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env sh
+
 build_function() {
     rustup update
     cargo install cross
-
 
     echo -n "Building x86_64-linux-gnu"
     cross build --release --target x86_64-unknown-linux-gnu
@@ -28,27 +28,27 @@ package_function() {
     mkdir aarch64-unknown-linux-gnu/
     mkdir aarch64-unknown-linux-musl/
 
-    cp ../target/x86_64-unknown-linux-gnu/release/topgrade      x86_64-unknown-linux-gnu/
-    cp ../target/x86_64-unknown-linux-musl/release/topgrade     x86_64-unknown-linux-musl/
-    cp ../target/x86_64-unknown-freebsd/release/topgrade        x86_64-unknown-freebsd/topgrade
-    cp ../target/aarch64-unknown-linux-gnu/release/topgrade     aarch64-unknown-linux-gnu/topgrade
-    cp ../target/aarch64-unknown-linux-musl/release/topgrade    aarch64-unknown-linux-musl/topgrade
-    cp ../target/x86_64-pc-windows-gnu/release/topgrade.exe     x86_64-pc-windows-gnu/topgrade.exe
+    cp ../target/x86_64-unknown-linux-gnu/release/topgrade x86_64-unknown-linux-gnu/
+    cp ../target/x86_64-unknown-linux-musl/release/topgrade x86_64-unknown-linux-musl/
+    cp ../target/x86_64-unknown-freebsd/release/topgrade x86_64-unknown-freebsd/topgrade
+    cp ../target/aarch64-unknown-linux-gnu/release/topgrade aarch64-unknown-linux-gnu/topgrade
+    cp ../target/aarch64-unknown-linux-musl/release/topgrade aarch64-unknown-linux-musl/topgrade
+    cp ../target/x86_64-pc-windows-gnu/release/topgrade.exe x86_64-pc-windows-gnu/topgrade.exe
 
     cd x86_64-unknown-linux-gnu/
     tar -czf ../topgrade-${ans}-x86_64-linux-gnu.tar.gz topgrade
     cd ..
 
     cd x86_64-unknown-linux-musl
-    tar -czf ../topgrade-${ans}-x86_64-linux-musl.tar.gz  topgrade
+    tar -czf ../topgrade-${ans}-x86_64-linux-musl.tar.gz topgrade
     cd ..
 
     cd x86_64-unknown-freebsd/
-    tar -czf ../topgrade-${ans}-x86_64-freebsd.tar.gz     topgrade
+    tar -czf ../topgrade-${ans}-x86_64-freebsd.tar.gz topgrade
     cd ..
 
     cd aarch64-unknown-linux-gnu/
-    tar -czf ../topgrade-${ans}-aarch64-linux-gnu.tar.gz  topgrade
+    tar -czf ../topgrade-${ans}-aarch64-linux-gnu.tar.gz topgrade
     cd ..
 
     cd aarch64-unknown-linux-musl/
@@ -56,14 +56,13 @@ package_function() {
     cd ..
 
     cd x86_64-pc-windows-gnu/
-    zip -q ../topgrade-${ans}-x86_64-windows.zip           topgrade.exe
+    zip -q ../topgrade-${ans}-x86_64-windows.zip topgrade.exe
     cd ..
     cd ..
 
 }
 
 print_checksums() {
-
 
     cd build/
     sha256sum topgrade-${ans}-*
@@ -72,21 +71,25 @@ print_checksums() {
 
 while true; do
 
-echo "You should always have a look on scripts you download from the internet."
-read -p "Do you still want to proceed? (y/n) " yn
+    echo "You should always have a look on scripts you download from the internet."
+    read -p "Do you still want to proceed? (y/n) " yn
 
-echo -n "Input version number: "
-read ans
-mkdir build
+    echo -n "Input version number: "
+    read ans
+    mkdir build
 
-case $yn in
-	y ) build_function
+    case $yn in
+    y)
+        build_function
         package_function
         print_checksums
-		break;;
-	n ) echo exiting...;
-		exit;;
-	* ) echo invalid response;;
-esac
+        break
+        ;;
+    n)
+        echo exiting...
+        exit
+        ;;
+    *) echo invalid response ;;
+    esac
 
 done
